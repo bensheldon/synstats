07aa9984dc20f7cd0db77e9231a809f622e64ee1
Ben Sheldon <bensheldon@gmail.com>
2012-07-06 22:43:00 -0700

    Made update-commit fully async. Added the new functional+todo patterns to view.

--- scripts/update-commits.js
+++ scripts/update-commits.js
@@ -12,45 +12,56 @@ var AuthorCount = require('../models/authorcount.js');
 var DailyCount = require('../models/dailycount.js');
 var Commit = require('../models/commit.js');
 
-
 var more = true;
 var skip = 0;
 var limit = 10;
 
 async.whilst(
-    function () { 
-      console.log('Updating the', (skip), 'th commit.');
-      return more; 
-    },
-    function (done) {
-      Commit
-            .find()
-            .where({"files": {"$exists": 1}})
-            .limit(limit)
-            .skip(skip)
-            .run(function(err, commits) {
-        if(!commits.length) {
-          more = false;
-          done();
+  function () { 
+    console.log('Updating the', (skip), 'th commit.');
+    return more; 
+  },
+  function (whilstDone) {
+    Commit.find()
+          .where({"files": {"$exists": 1}})
+          .limit(limit)
+          .skip(skip)
+          .run(function(err, commits) {
+      
+      // if no more, end the whilst;
+      if (!commits.length) { more = false; whilstDone(); }
+      // go through each commit and save it
+      async.forEach(
+        commits
+      , function(commit, done) {
+          commit.countPatterns().save(function(err) {
+            if (err) { console.log("Bad commit!", err, commit) };
+            done();
+          });
         }
-        else {
-          for(var i = 0; i < commits.length; i++) {
-            try {
-            commits[i].countPatterns().save();
-            }
-            catch(err) {console.log(commits[i])} // print out anything that doesn't go in
-          }
+      , function(err) {
           skip += limit;
-          done();
+          whilstDone();
         }
-      })
-    },
-    function (err) {
-      console.log('All commits have been updated');
-      // TODO: MAKE THIS ASYNC!
-      AuthorCount.generate(function(err, results) {});
-      DailyCount.generate(function(err, results) {});
-    }
+      )            
+    });
+  },
+  function (err) {
+    // now do our map-reduce counts
+    async.series([
+      function(done){
+        AuthorCount.generate(function(err, results) { if(err) {console.log(err) }; done() });
+      },
+      function(done){
+        DailyCount.generate(function(err, results) { if(err) {console.log(err) }; done() });
+      },
+    ],
+    function(err){
+      // Whew! All done.
+      console.log('All commits and map-reduce aggregations have been updated!');
+      process.exit();
+    });
+  }
 );
 
 
