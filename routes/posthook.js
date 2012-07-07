d4700defe6d1c52276aac5cc58320c41cc624576
Ben Sheldon <bensheldon@gmail.com>
2012-07-06 20:00:01 -0700

    Fixed where the map-reduces go after a push (still could be cleaner)

--- routes/posthook.js
+++ routes/posthook.js
@@ -1,16 +1,23 @@
-// var async = require('async');
+var async = require('async');
 
 var Push = require('../models/push.js');
+var DailyCount = require('../models/dailycount.js');
+var AuthorCount = require('../models/authorcount.js');
 
 module.exports = function(req, res) {
   // store whatever github sends us
   var push = new Push( JSON.parse(req.body.payload) );
   push.getCommits(function(commits) {
-    // do something with the commits
-    commits.map(function(commit) {
-      // pattern matching and counting; then save
-      commit.countPatterns().save();
-    });
+    async.forEach(
+      commits
+    , function(commit, done) {
+        commit.countPatterns().save(function(err, commit){ done() });
+      }
+    , function(err) {
+        AuthorCount.generate(function(err, results) {console.log("Updated AuthorCounts")});
+        DailyCount.generate(function(err, results) {console.log("Updated DailyCounts")});
+      }
+    );
   });
   push.save();
 
